683a9178ab5da132445035a6ccbfaac6758e732a
ZZZ <zxx457@ZZZs-MacBook-Pro.local>
2025-08-26 21:42:56 +0800

    clean up

--- project/src/lib/core/algorithms/filter.ts
+++ project/src/lib/core/algorithms/filter.ts
@@ -1,10 +1,10 @@
 import type { CompundFilterInterface } from "$lib/utils/rw";
-import type { Compound, CompoundA, CompoundB, Sample } from "../models/compund.model";
+import type { Sample } from "../models/compund.model";
 import type DataSets from "../models/datasets.model";
 import BasicFilter from "./BasicFilter";
 
 
-export default class Filter {
+export default class CompoundFilter {
     private bf: BasicFilter
     // specturm filter
 
@@ -15,7 +15,7 @@ export default class Filter {
                 this.bf = new BasicFilter(samples, datasets)
     }
 
-    extract(): DataSets {
+    extract(): Compound[] {
             
     }
 
