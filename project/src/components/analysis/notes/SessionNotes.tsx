729b34a22a149e931c64f70e96a5f977c7222e13
Gio <me@gio.blue>
2025-10-13 16:43:57 +0800

    ux: hide Save Notes button, we do it automatically

--- project/src/components/analysis/notes/SessionNotes.tsx
+++ project/src/components/analysis/notes/SessionNotes.tsx
@@ -23,9 +23,9 @@ export default function SessionNotes({ session, storage }: Props) {
                     value={storage.data ?? ""}
                     onChange={(e) => updateData(e.currentTarget.value)}
                 ></textarea>
-                <button className="btn-hover-effect mt-3 w-full rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600">
+                {/*<button className="btn-hover-effect mt-3 w-full rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600">
                     Save Notes
-                </button>
+                </button>*/}
             </div>
         </>
     );
